674809d21c9ab9ab0d91c9a635f8128410ea2c0f
Ishaan Raj MIshra <55981156+IshaanRajMishra@users.noreply.github.com>
2021-10-06 00:26:21 +0530

    Add files via upload

---
+++ maxGcd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int n;
+    cin >> n;
+    int arr;
+    int ar2;
+    for (int i=0;i<n;i++){
+
+        cin >>arr;
+        ar2=arr/2;
+        cout << ar2 << "\n";
+    }
+
+    return 0;
+ 
+}
